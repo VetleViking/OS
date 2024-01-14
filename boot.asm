e43e57c6fc7758b5b008d7422d8632761baef79f
VetleViking <vetlelind@gmail.com>
2024-01-14 03:04:26 +0100

    prormaing

--- boot.asm
+++ boot.asm
@@ -1,6 +1,8 @@
+
+
+
 [org 0x7c00]
 mov ah, 0x0e
-
 
 mov bl, 40
 mov al, 61
@@ -15,7 +17,7 @@ mov al, 13
 int 0x10
 
 mov al, 32
-mov bl, 9
+mov bl, 18
 loop1:
     int 0x10
     dec bl
@@ -61,8 +63,6 @@ osDescStop:
     int 0x10
 
 
-
-
 mov bl, 40
 mov al, 61
 loop2:
@@ -75,6 +75,81 @@ int 0x10
 mov al, 13
 int 0x10
 
+mov bl, 10
+test: 
+    mov ax, 0
+    int 0x16
+    mov ah, 0x0e
+    int 0x10
+    dec bl
+    cmp bl, 0
+    jg test
+
+mov al, 10
+int 0x10
+mov al, 13
+int 0x10
+
+mov ax, 0
+int 0x16
+
+mov [char], al
+
+mov ah, 0x0e
+int 0x10
+
+mov al, 10
+int 0x10
+mov al, 13
+int 0x10
+
+endFunc:
+
+
+mov ax, 0
+int 0x16
+mov ah, 0x0e
+int 0x10
+
+cmp al, 13
+je newLine
+
+cmp al, 97
+je aFunc
+
+cmp al, 110
+je nFunc
+
+jne endFunc
+    
+aFunc:
+    mov ah, 0x0e
+    mov al, [char]
+    int 0x10
+    
+    jmp endFunc  
+
+nFunc: 
+    mov ah, 0x0e
+    mov bx, nameOs
+printName:
+    mov al, [bx]
+    cmp al, 0
+    je endFunc
+    int 0x10
+    inc bx
+    jmp printName
+
+newLine:
+    mov ah, 0x0e
+    mov al, 10
+    int 0x10
+    mov al, 13
+    int 0x10
+    jmp endFunc
+
+char:
+    db 0
 
 nameOs:
     db 'OSU', 0
