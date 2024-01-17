b4d48d1edd801fe7f07ad831fc861d324beac4e6
VetleViking <vetlelind@gmail.com>
2024-01-17 23:59:41 +0100

    cat (meow)

--- makefile
+++ makefile
@@ -1,20 +1,16 @@
-step6: step5
+startQemu: compileEverything
 	cd src && qemu-system-x86_64 everything.bin
 
-step5: step4
+combineEverything: compileBoot
 	cd src && cat "boot.bin" "full_kernel.bin" > "everything.bin"
 
-step4: step3
+compileBoot: combineKernel
 	cd src && nasm "boot2.asm" -f bin -o "boot.bin"
 
-step3: step2a
+combineKernel: compileKernel
 	cd src && i386-elf-ld -o "full_kernel.bin" -Ttext 0x1000 "kernel_entry.o" "kernel.o" "kernel2.o" --oformat binary
 
-step2a: step2
-	cd src && nasm "kernel.asm" -f elf -o "kernel2.o"
-
-step2: step1
-	cd src && nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
-
-step1:
+compileKernel: 
 	cd src && i386-elf-gcc -ffreestanding -m32 -g -c "kernel.c" -o "kernel.o"
+	cd src && nasm "kernel.asm" -f elf -o "kernel2.o"
+	cd src && nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
--- test/kernel.c
+++ test/kernel.c
@@ -53,7 +53,7 @@ void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_MAGENTA, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -94,11 +94,32 @@ void terminal_writestring(const char* data)
 {
 	terminal_write(data, strlen(data));
 }
+
+void newline()
+{
+	terminal_row++;
+	terminal_column = 0;
+}
  
 void kernel_main(void) 
 {
 	terminal_initialize();
- 
-	terminal_writestring("Hello World!");
-	terminal_writestring("Hello World!2");
+	terminal_writestring("  _____");
+	newline();
+    terminal_writestring(" |     |___ ___ _ _ _                 |\\__/,|   (`\\");
+    newline();
+    terminal_writestring(" | | | | -_| . | | | |              _.|o o  |_   ) )");
+    newline();
+    terminal_writestring(" |_|_|_|___|___|_____|             -(((---(((--------");
+
+	
+	// Cat (Meow)
+	// 
+	//         |\__/,|   (`\
+  	//	     _.|o o  |_   ) )
+	//	   -(((---(((--------
+	//  _____               
+	// |     |___ ___ _ _ _ 
+    // | | | | -_| . | | | |
+	// |_|_|_|___|___|_____|
 }
