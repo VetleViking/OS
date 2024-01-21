1d9742d999619e67597fce81a03ca43a7ae66636
VetleViking <vetlelind@gmail.com>
2024-01-21 14:56:03 +0100

    finished tic tac toe game

--- test/kernel.c
+++ test/kernel.c
@@ -260,7 +260,38 @@ void end_tic_tac_toe() {
 	in_game = false;
 }
 
-// Tic tac toe game (not finished)
+
+int check_for_win_ttt() {
+	for (size_t i = 0; i < 5; i = i + 2) {
+		if (tic_tac_toe_board[i][0] == tic_tac_toe_board[i][2] && tic_tac_toe_board[i][2] == tic_tac_toe_board[i][4]) {
+			if (tic_tac_toe_board[i][0] == 'X') {
+				return 1;
+			} else if (tic_tac_toe_board[i][0] == 'O') {
+				return 2;
+			}
+		}
+
+		if (tic_tac_toe_board[0][i] == tic_tac_toe_board[2][i] && tic_tac_toe_board[2][i] == tic_tac_toe_board[4][i]) {
+			if (tic_tac_toe_board[0][i] == 'X') {
+				return 1;
+			} else if (tic_tac_toe_board[0][i] == 'O') {
+				return 2;
+			}
+		}
+	}
+	if ((tic_tac_toe_board[0][0] == tic_tac_toe_board[2][2] && tic_tac_toe_board[2][2] == tic_tac_toe_board[4][4]) || (tic_tac_toe_board[0][4] == tic_tac_toe_board[2][2] && tic_tac_toe_board[2][2] == tic_tac_toe_board[4][0])) {
+		if (tic_tac_toe_board[2][2] == 'X') {
+			return 1;
+		} else if (tic_tac_toe_board[2][2] == 'O') {
+			return 2;
+		}
+	}
+	
+	return 0;
+}
+
+
+// Tic tac toe game TODO: make the pc better at the game
 void tic_tac_toe() {
 	if (game_round == 1) {
 		terminal_writestring("Tic tac toe, you go first!\n");
@@ -275,15 +306,14 @@ void tic_tac_toe() {
 		int x = command[0] - '0';
 		int y = command[2] - '0';
 
-		
-
 		if (x == 1) {
 			x = 0;
 		} else if (x == 3) {
 			x = 4;
 		} else if (x != 2) {
-			terminal_writestring("That is not a valid move, You lose!");
-			end_tic_tac_toe();
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("That is not a valid move, Try again.");
 			return;
 		}
 
@@ -292,18 +322,35 @@ void tic_tac_toe() {
 		} else if (y == 3) {
 			y = 4;
 		} else if (y != 2) {
-			terminal_writestring("That is not a valid move, You lose!");
-			end_tic_tac_toe();
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("That is not a valid move, Try again.");
 			return;
 		}
 
 		if (tic_tac_toe_occupied[x][y] == 1) {
-			terminal_writestring("That is not a valid move, Try again.");
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("That place is occupied, Try again.");
 			return;
 		}
 		tic_tac_toe_board[x][y] = 'X';
 		tic_tac_toe_occupied[x][y] = 1;
 
+		check_for_win_ttt();
+		if (check_for_win_ttt() == 1) {
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("You win!");
+			end_tic_tac_toe();
+			return;
+		} else if (check_for_win_ttt() == 2) {
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("You lose!");
+			end_tic_tac_toe();
+			return;
+		}
 
 		bool placed = false;
 		for (size_t i = 0; i < 5; i = i + 2) {
@@ -321,9 +368,30 @@ void tic_tac_toe() {
 				break;
 			}
 		}
+		if (!placed) {
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("Its a tie!");
+			end_tic_tac_toe();
+			return;
+		}
+
+		check_for_win_ttt();
+		if (check_for_win_ttt() == 1) {
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("You win!");
+			end_tic_tac_toe();
+			return;
+		} else if (check_for_win_ttt() == 2) {
+			print_tic_tac_toe_board();
+			newline();
+			terminal_writestring("You lose!");
+			end_tic_tac_toe();
+			return;
+		}
 
 		print_tic_tac_toe_board();
-
 		newline();
 		terminal_writestring("Your turn.");
 		game_round++;
